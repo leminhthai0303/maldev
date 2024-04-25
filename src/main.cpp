e87755aaf876b6d93ceec4b400533b665d1337ef
ng0cph0ng <fogsbuddha@gmail.com>
2024-04-25 22:11:43 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@ int main()
 {
     uint8_t key[16];
     uint8_t iv[16];
-    generateKeyAndIV(key, iv, SIZE);
+    genKeyIfNeeded(key, iv);
+
+    readFromFile(keyFilename, key, SIZE);
+    readFromFile(ivFilename, iv, SIZE);
 
     std::string path = "C:\\test\\";
     std::vector<FileItem> items = listFilesAndDirectories(path);
-    print(items, "", path, key, iv);
+    enc(items, "", path, key, iv);
     return 0;
 }
